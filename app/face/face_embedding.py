d4574b03e7fee094983a56b9a4e52ab22ed639b2
willook <jonghwa@actnova.io>
2025-05-19 08:40:19 +0900

    회원 삭제 기능에서 “삭제된 회원은 조회할 수 없다.”에 해당하는 테스트 코드를 구현

--- app/face/face_embedding.py
+++ app/face/face_embedding.py
@@ -1,8 +1,16 @@
 def extract_embedding(image):
     """이미지에서 임베딩 추출"""
     pass
+import numpy as np
+from typing import Union
+from deepface import DeepFace
 
+def extract_embedding(img_path: Union[str, np.ndarray]) -> list:
+    embedding = DeepFace.represent(img_path)[0]['embedding']
+    return embedding
 
-def verify_embedding(embedding1, embedding2):
+def verify_embedding(embedding1: list, embedding2: list) -> bool:
     """두 임베딩이 같은 사람인지 검증"""
     pass
+    is_same_person = DeepFace.verify(embedding1, embedding2)['verified']
+    return is_same_person
--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,54 @@
-from pydantic import BaseModel
+import cv2
+import numpy as np
+from datetime import datetime
+
+from app.face.face_embedding import *
+from app.face.face_db import *
+
+def register_user(user_id: str, image_bytes: bytes) -> dict:
+    """이미지와 ID로 회원 등록"""
+    pass
+    image_np = np.frombuffer(image_bytes, dtype=np.uint8)
+    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    embedding = extract_embedding(image)
+    save_user(user_id, embedding)
+    return {'user_id': user_id, 'registered_at': str(datetime.now())}
 
 
-class UserResponse(BaseModel):
-    """회원 응답 모델"""
+def authenticate_user(image_bytes: bytes):
+    """이미지로 회원 인증"""
+    pass
+    image_np = np.frombuffer(image_bytes, dtype=np.uint8)
+    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    db = load_db()
 
-    user_id: str
-    registered_at: str
+    embedding_to_check = extract_embedding(image)
+
+    for user_id, data in db.items():
+        if verify_embedding(data['embedding'], embedding_to_check):
+            return user_id
+
+    return None # 인증 실패
+
+
+def get_user(user_id):
+    """user_id로 회원 정보 조회"""
+    pass
+    db = load_db()
+    user_data = db.get(user_id)
+
+    if user_data:
+        return {'user_id': user_id, 'registered_at': user_data['registered_at']}
+    else:
+        return None # 사용자 없음
+
+
+def delete_user(user_id):
+    """user_id로 회원 삭제"""
+    pass
+    db = load_db()
+    if user_id in db:
+        del db[user_id]
+        save_db(db)
+        return True
+    return False
